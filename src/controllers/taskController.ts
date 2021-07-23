2286b0828fb9c5abf0a469b1f55d8df4892245d1
Lakhan Nad <lakhannad1999@gmail.com>
2021-07-23 14:04:07 +0530

    fixed post route

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,9 +1,8 @@
-import * as config from "../config";
 import { Request, Response, NextFunction } from "express";
 import _ from "lodash";
-import { BadRequestError } from "../errors";
+import { BadRequestError, InternalServerError } from "../errors";
 import { decrypt, encrypt } from "../utils/randoms";
-import { getTasks } from "../db/queries";
+import { getTasks, insertTask } from "../db/queries";
 
 export async function getAllTasks(
   _req: Request,
@@ -17,7 +16,7 @@ export async function getAllTasks(
   res.status(200).json(tasks);
 }
 
-export function addTask(req: Request, res: Response, next: NextFunction) {
+export async function addTask(req: Request, res: Response, next: NextFunction) {
   let { title, description, category, due_date } = req.body;
   if (!_.isDate(due_date)) {
     next(new BadRequestError("due date provided is invalid"));
@@ -26,4 +25,10 @@ export function addTask(req: Request, res: Response, next: NextFunction) {
   // store in ISO string format
   due_date = new Date(due_date).toISOString();
   description = encrypt(description);
+  let inserted = await insertTask(title, description, category, due_date);
+  if (inserted) {
+    res.status(200).json({ status: "success", status_code: 200 });
+    return;
+  }
+  next(new InternalServerError("task not inserted"));
 }
